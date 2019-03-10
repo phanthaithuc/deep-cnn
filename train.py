87516df234435b6d336b97932f7d1729aaf4d963
phanthaithuc <phanthai@arcada.fi>
2019-03-11 00:20:56 +0200

    test-output-#2

--- train.py
+++ train.py
@@ -92,9 +92,12 @@ def train():
         if os.path.isdir(FLAGS.log_path):
             shutil.rmtree(FLAGS.log_path)
         os.makedirs(FLAGS.log_path)
-        #if os.path.isdir(FLAGS.saved_path):
+        if os.path.isdir(FLAGS.saved_path):
             #shutil.rmtree(FLAGS.saved_path)
-        #os.makedirs(FLAGS.saved_path)
+        os.makedirs(FLAGS.saved_path)
+        outputs_dir = os.getenv('VH_OUTPUTS_DIR')
+        output_files = os.path.join(outputs_dir, "log.txt", "w")
+        np.savetxt(output_files, delimiter=",")
         output_file = open(FLAGS.saved_path + os.sep + "logs.txt", "w")
         output_file.write("Model's parameters: {}".format(
             FLAGS.flag_values_dict()))
