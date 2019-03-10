a79561369c393392b94b8b9d2dace1b3201abe3c
phanthaithuc <phanthai@arcada.fi>
2019-03-11 00:27:53 +0200

    fixed-#3

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ def train():
         os.makedirs(FLAGS.saved_path)
         outputs_dir = os.getenv('VH_OUTPUTS_DIR')
         output_files = os.path.join(outputs_dir, "log.txt", "w")
-        np.savetxt(output_files, delimiter=",")
+        np.savetxt(output_files, ws.eval(), delimiter=",")
         output_file = open(FLAGS.saved_path + os.sep + "logs.txt", "w")
         output_file.write("Model's parameters: {}".format(
             FLAGS.flag_values_dict()))
